a656d0eb324408a5f48789c41120adcc5ecad808
likamatisz <60703499+likamatisz@users.noreply.github.com>
2020-02-06 09:36:41 +0300

    First commit

---
+++ FirstAppGit.py
@@ -0,0 +1,13 @@
+# First app in Git.
+# Math operation and print result.
+firstNum = 1
+secontNum = 2
+sum = firstNum + secontNum
+delet = firstNum / secontNum
+multip = firstNum * secontNum
+exp = firstNum**secontNum
+div = firstNum // secontNum
+mod = firstNum % secontNum
+print("Первое число:", firstNum, "Второе число:", secontNum, "Сумма:", sum, "Деление:", delet)
+print("Возведение в степень:", exp, "Умножение:", multip)
+print("Целое число от деление:", div, "Остаток от деления:", mod)
